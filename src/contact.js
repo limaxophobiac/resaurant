4165c7a01b666c0450eff23a26c2e2b3527f64c6
limax <joel_bolkert@hotmail.com>
2022-05-13 19:41:43 +0200

    contact looking better

--- src/contact.js
+++ src/contact.js
@@ -5,25 +5,43 @@ import pizzaMap from './pizzaMap.png';
 const buildContact = () => {
     const contentDiv = document.getElementById('content');
     const contactDiv = document.createElement('div');
-    contactDiv.style.backgroundImage = `url("${backgroundPlace}")`;;
+    contactDiv.style.backgroundImage = `url("${backgroundPlace}")`;
+    contactDiv.style.backgroundAttachment = "fixed";
+    contactDiv.style.backgroundSize = "cover";
+    contactDiv.style.backgroundPosition = "center";
     contactDiv.style.minHeight = "100vh";
+
+    contactDiv.style.display = "flex";
+    contactDiv.style.alignItems = "center";
+    contactDiv.style.justifyContent = "center";
+
+    const contactBox = document.createElement('div');
+    contactBox.style.backgroundColor = "rgba(0,0,0, 0.25)";
+    contactBox.style.padding = "2.5vh 5vw 5vh 5vw";
+    contactBox.style.color = "white";
+    contactBox.style.borderRadius = "10px";
+    contactBox.style.marginTop = "120px";
 
     const contactTitle = document.createElement('div');
     contactTitle.innerHTML = "Contact Us";
     contactTitle.style.fontSize = "2rem";
-    contactDiv.appendChild(contactTitle);
+    contactBox.appendChild(contactTitle);
 
     const contactNumber = document.createElement('div');
     contactNumber.innerHTML = "12-345 67 89";
-    contactDiv.appendChild(contactNumber);
+    contactBox.appendChild(contactNumber);
 
     const contactEmail = document.createElement('div');
     contactEmail.innerHTML = "fake@email.com";
-    contactDiv.appendChild(contactEmail);
+    contactBox.appendChild(contactEmail);
 
     const mapImage = new Image();
     mapImage.src  = pizzaMap;
-    contactDiv.appendChild(mapImage);
+    mapImage.style.maxWidth = "60vw";
+    mapImage.style.maxHeight = "45vh";
+    contactBox.appendChild(mapImage);
+
+    contactDiv.appendChild(contactBox);
 
     contentDiv.appendChild(contactDiv);
 };
