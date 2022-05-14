2fc83cda53d3c0580b1249d0bb64fb0326af4228
limax <joel_bolkert@hotmail.com>
2022-05-14 23:54:54 +0200

    done for now

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,26 @@
+import backgroundMenu from './1390893.jpg';
+
 const buildMenu = () => {
     const contentDiv = document.getElementById('content');
+
     const menuDiv = document.createElement('div');
-    menuDiv.style.backgroundColor = "blue";
+
+    menuDiv.style.backgroundImage = `url("${backgroundMenu}")`;
+    menuDiv.style.backgroundAttachment = "fixed";
+    menuDiv.style.backgroundSize = "cover";
+    menuDiv.style.backgroundPosition = "center";
+    menuDiv.style.color = "white";
+    menuDiv.style.fontFamily = "Helvetica";
+
     menuDiv.style.minHeight = "100vh";
+
+    menuDiv.style.display = "flex";
+    menuDiv.style.alignItems = "center";
+    menuDiv.style.justifyContent = "center";
 
     const menuTitle = document.createElement('div');
     menuTitle.innerHTML = "Menu";
-    menuTitle.style.fontSize = "2rem";
+    menuTitle.style.fontSize = "3rem";
 
     menuDiv.appendChild(menuTitle);
 
